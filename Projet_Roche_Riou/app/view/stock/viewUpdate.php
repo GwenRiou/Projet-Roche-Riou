6e9948c40175451ef7c21e682d43d5088eca9a28
GwenRiou <74981788+GwenRiou@users.noreply.github.com>
2021-06-28 22:55:26 +0200

    Update viewUpdate.php

--- Projet_Roche_Riou/app/view/stock/viewUpdate.php
+++ Projet_Roche_Riou/app/view/stock/viewUpdate.php
@@ -19,24 +19,17 @@ require ($root . '/app/view/fragment/fragmentCaveHeader.html');
         <input type="hidden" name='action' value='<?php echo($target);?>'>
         <input type="hidden" name='label' value='<?php echo($_GET["label"])?>'>
         <br>
-        <label>Choississez le nombre de doses à ajouter par vaccin : </label>
+        <label>Choississez le nombre de doses à ajouter par vaccin (si le nombre entré est négatif, le nombre de doses du vaccin correspondant diminuera de la valeur choisie) : </label>
         <br>
         <?php
             foreach ($results as $key1 => $value1) {
-                //foreach ($value1 as $key2 => $value2) {
-                    //echo ("$key2");
-                    //if ($key2 == "label") {
-                    $label_vaccin = $value1["label"];
-                    $id_vaccin = $value1["vaccin_id"];
-                        echo ("<br>$label_vaccin");
-                        echo ('<br><label for="doses">doses : </label>');
-                        //echo ("<input name='$id_vaccin' type='number' value='0'>");
-                        echo ("<input name='$id_vaccin' type='number' value='0'>");
-                    //}
-                //}
+                $label_vaccin = $value1["label"];
+                $id_vaccin = $value1["vaccin_id"];
+                echo ("<br>$label_vaccin");
+                echo ('<br><label for="doses">doses : </label>');
+                echo ("<input name='$id_vaccin' type='number' value='0'>");
             }
             echo ("<br>");
-            //print_r($results);
         ?>
       </div>
       <p/>
