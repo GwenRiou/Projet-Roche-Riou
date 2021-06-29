102beb49a38236dfb39ed831ff83e429b472a897
Rochijr <71018576+Rochijr@users.noreply.github.com>
2021-06-29 15:45:41 +0200

    Update documentation3.php

--- Projet_Roche_Riou/public/documentation/documentation3.php
+++ Projet_Roche_Riou/public/documentation/documentation3.php
@@ -1,4 +1,4 @@
-<h3>Innovation 3 : Suppression d'un centre</h3>
+<h2>Innovation 3 : Suppression d'un centre</h2>
 <p>
     Cette innovation permet de supprimer un centre si tous les vaccins du centre n'ont plus aucune doses (quantité = 0).<br>
     L'utilisateur choisit un centre à supprimer, nous allons grâce à des requêtes vérifier si tous les vaccins dans ce centre ont une quantité égale à 0, auquel cas nous pourrons supprimer le "centre_id" et tout ce qui est lié dans la table "stock" ainsi que le centre dans la table "centre".<br>
