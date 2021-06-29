0e09c0b57e62b96ffe9b52c96fd4ac0bd07ff92d
Rochijr <71018576+Rochijr@users.noreply.github.com>
2021-06-29 15:37:10 +0200

    Update documentation2.php

--- Projet_Roche_Riou/app/view/Documentation/documentation2.php
+++ Projet_Roche_Riou/app/view/Documentation/documentation2.php
@@ -5,13 +5,9 @@
     <?php
     include $root. '/app/view/fragment/fragmentCaveMenu.html';
     include $root.'/app/view/fragment/fragmentCaveJumbotron.html';
-    ?>  
-        <h3>Innovation 2 : Réapprovisionnement des stocks</h3>
-        <p>
-            Cette innovation permet d'afficher les vaccins des centres ayant une quantité inférieure à un nombre donné par l'utilisateur.<br>
-            L'utilisateur entre une valeur, admettons 10, puis nous allons rechercher tous les vaccins ayant moins de 10 doses (quantité), nous allons alors afficher dans un tableau le nom des vaccins pour chaque centre avec une quantité inférieure à 10 (dans notre cas).<br>
-            Nous avons déjà une requête permettant l'ajout de doses, cependant nous avons pensé qu'il serait utile de pouvoir observer quels sont les vaccins n'ayant plus assez de doses (et nous laissons l'utilisateur définir la limite).
-        </p>
+   
+    include $root.'/public/documentation/documentation1.php';
+    ?>
     </div>
   <?php
   include $root. '/app/view/fragment/fragmentCaveFooter.html';
