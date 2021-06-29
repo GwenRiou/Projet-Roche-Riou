dceaf2b56aa77696e885e766d4821518f17062fd
Rochijr <71018576+Rochijr@users.noreply.github.com>
2021-06-29 15:45:20 +0200

    Update documentation2.php

--- Projet_Roche_Riou/public/documentation/documentation2.php
+++ Projet_Roche_Riou/public/documentation/documentation2.php
@@ -1,4 +1,4 @@
-<h3>Innovation 2 : Réapprovisionnement des stocks</h3>
+<h2>Innovation 2 : Réapprovisionnement des stocks</h2>
 <p>
     Cette innovation permet d'afficher les vaccins des centres ayant une quantité inférieure à un nombre donné par l'utilisateur.<br>
     L'utilisateur entre une valeur, admettons 10, puis nous allons rechercher tous les vaccins ayant moins de 10 doses (quantité), nous allons alors afficher dans un tableau le nom des vaccins pour chaque centre avec une quantité inférieure à 10 (dans notre cas).<br>
